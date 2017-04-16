76535886c3da0a144d56bbedf8daff63fd029496
Akshat Goel <akshat.goel@idinsight.org>
2017-04-16 15:50:42 +0530

    Adding code that downloads rainfall .pdfs
    This code downloads the .pdfs containing daily rainfall data from 1957

---
+++ RJ_GetPDF.py
@@ -0,0 +1,49 @@
+#--------------------------------#
+#------Akshat Goel---------------# 
+#------IDinsight-----------------# 
+#------Thursday, March 25th 2017-#
+#--------------------------------#
+
+## Importing relevant libraries 
+import requests, os, time
+
+## Beginning timer
+start_time = time.time()
+
+## Description:
+## This function takes as the URL of the .pdfs containing daily Rajasthan rainfall data 
+## It creates a directory to store the files
+## It writes each file to the created directory
+
+def downloadRain(year, end_year):
+	
+	## Storing current working directory 
+	current_dir = "/Users/Akshat/Desktop/RJ_Rainfall/"
+
+	while not year > end_year: 
+		
+		## Get PDF and store as a response object
+		res = requests.get("http://www.water.rajasthan.gov.in/content/dam/water/water-resources-department/AnnualRainfall/Print%20" + str(format(year, '04d')) + ".pdf")
+		type(res)  
+		res.status_code == requests.codes.ok
+	
+		## Write PDF to hard drive
+		with open( current_dir + str(format(year, '04d')) + ".pdf", 'wb') as f:
+			
+			## Write command
+			f.write(res.content)
+    
+    	#Update year
+		year = year + 1 
+		
+		
+#Testing function 
+
+downloadRain(1957,2015)
+print("Done!")
+
+
+#Timing function
+print(time.time() - start_time)
+
+
